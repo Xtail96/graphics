8527bca3d095849848830380da20a7d5ed9645f1
Xtail <xtail1996@yandex.ru>
2018-05-26 11:57:29 +0300

    Сглаживаение текстур

--- OpenGL/OGL/main.cpp
+++ OpenGL/OGL/main.cpp
@@ -4,6 +4,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSurfaceFormat format;
+    format.setSamples(16);
+    format.setDepthBufferSize(24);
+    QSurfaceFormat::setDefaultFormat(format);
+
     Widget w;
     w.show();
 
